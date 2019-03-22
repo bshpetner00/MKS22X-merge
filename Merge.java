3e5dd247c321c5da32e25eac3a9ef40cb38a4800
Ben Shpetner <bshpetner00@stuy.edu>
2019-03-22 01:03:16 -0400

    made some minor edits for readability and efficiency

--- Merge.java
+++ Merge.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Merge {
 
 	private static void swap(int[]data, int i0,int v0, int i1, int v1) {
@@ -5,29 +7,56 @@ public class Merge {
 		data[i1] = v0;
 	}
 
-	public static void mergesort(int[]data) {
-		mergeH(data,0,data.length-1);
-	}
-	public static void mergeH(int[]data, int lo, int hi) {
-		if (lo >= hi) {
-			return;
+	private static int medianI(int[]data, int start, int end) {
+		int mid = (end+start)/2;
+		if ((start - end) * (mid - start) >= 0) { // a >= b and a <= c OR a <= b and a >= c
+			return start;
 		}
-		int piv = (hi - lo) / 2 + lo;
-		mergeH(data, lo, piv);
-		mergeH(data, piv+1, hi);
-		merge(data,lo,piv+1,hi);
-	}
-	public static void merge(int[]data,int lo, int secondHalf, int hi) {
-		if (lo + 1 == hi) {
-			if (data[lo] > data[hi]) {
-				swap(data, lo, data[lo], hi, data[hi]);
-			}
+		else if ((end - start) * (mid - end) >= 0) {// b >= a and b <= c OR b <= a and b >= c
+			return end;
 		}
 		else {
-			int[] temp = data;
-			int iOne = 0;
-			int iTwo = 0;
+			return mid;
+		}
+	}
 
+	private static void insertionSort(int[]data, int lo, int hi) {
+		return;
+	}
+
+	public static void mergesort(int[]data) {
+		int[] temp = Arrays.copyOf(data, data.length);
+		mergeH(data,temp,0,data.length-1);
+
+	}
+	public static void mergeH(int[]data, int[]temp, int lo, int hi) {
+		if (hi - lo <= 47) {
+			insertionSort(data,lo,hi);
+		}
+		else if (lo >= hi) {
+			return;
+		}
+		else {
+			int piv = medianI(data,lo,hi);
+			mergeH(data, temp, lo, piv);
+			mergeH(data, temp, piv+1, hi);
+			merge(data,temp,lo,piv,hi);
+		}
+	}
+
+	public static void merge(int[]data,int[]temp, int lo, int piv, int hi) {
+		int iOne = 0;
+		int iTwo = 0;
+		while (iOne <= piv-low && iTwo < hi-piv) {
+			if (temp[lo+iOne] >= temp[mid+(iTwo+1)]) {
+				data[lo+iOne+iTwo] = temp[mid+(iTwo+1)];
+				iTwo++;
+				
+			}
+			else {
+				data[lo+iOne+iTwo] = temp[lo+iOne];
+				iOne++;
+			}
 		}
 	}
 }
