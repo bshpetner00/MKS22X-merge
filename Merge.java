e158617f85e1129952f642c608dc9503a042c39a
Ben Shpetner <bshpetner00@stuy.edu>
2019-03-22 02:26:49 -0400

    ready to do insertion

--- Merge.java
+++ Merge.java
@@ -2,27 +2,12 @@ import java.util.*;
 
 public class Merge {
 
-	private static void swap(int[]data, int i0,int v0, int i1, int v1) {
-		data[i0] = v1;
-		data[i1] = v0;
+	private static void swap(int[]ary, int f, int l) {
+		int temp = ary[f];
+		ary[f] = ary[l];
+		ary[l] = temp;
 	}
 
-	private static int medianI(int[]data, int start, int end) {
-		int mid = (end+start)/2;
-		if ((start - end) * (mid - start) >= 0) { // a >= b and a <= c OR a <= b and a >= c
-			return start;
-		}
-		else if ((end - start) * (mid - end) >= 0) {// b >= a and b <= c OR b <= a and b >= c
-			return end;
-		}
-		else {
-			return mid;
-		}
-	}
-
-	private static void insertionSort(int[]data, int lo, int hi) {
-		return;
-	}
 
 	public static void mergesort(int[]data) {
 		int[] temp = Arrays.copyOf(data, data.length);
@@ -30,16 +15,17 @@ public class Merge {
 
 	}
 	public static void mergeH(int[]data, int[]temp, int lo, int hi) {
-		if (hi - lo <= 47) {
-			insertionSort(data,lo,hi);
-		}
 		if (lo < hi) {
-			int piv = lo + (hi - lo) / 2;
-			mergeH(temp, data, lo, piv);
-			mergeH(temp, data, piv + 1, hi);
-			merge(data, temp, lo, piv, hi);
+			if (hi - lo <= 47) {
+				insertionSort(data, lo, hi);
+			}
+			else {
+				int piv = lo + (hi - lo) / 2;
+				mergeH(temp, data, lo, piv);
+				mergeH(temp, data, piv + 1, hi);
+				merge(data, temp, lo, piv, hi);
+			}
 		}
-
 	}
 
 	private static void cleanUp(int[]data, int[]temp, int iOne, int iTwo, int lo, int piv, int hi) {
